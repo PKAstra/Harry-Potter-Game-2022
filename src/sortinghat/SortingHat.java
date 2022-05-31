356cb4fed86608513f68b0284beb4b7bb55fba76
AjayRamkissoon <ajayramkissoon@gmail.com>
2022-05-30 23:21:08 -0400

    brand new app

--- src/sortinghat/SortingHat.java
+++ src/sortinghat/SortingHat.java
@@ -4,22 +4,17 @@ package sortinghat;
  *
  * @author William
  */
-import java.util.Scanner;
 public class SortingHat {
 
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
-        Screen screen = new Screen();
-        String studentName;       
         
-        System.out.println("What's your name young they/them?");
-        studentName = scnr.nextLine();
-        System.out.println(studentName + ", is it?");
-        Houses.gryffindor(studentName);
+        Screen screen = new Screen();
+        
+        Houses.welcome();
+        Houses.askName();
         
         screen.pause();           
-    }
-          
+    }  
 }
   
 
